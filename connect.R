dfbd6d89ba87cf5c53903c2dc734d50d0844cb90
aedobbyn <adobbs11@gmail.com>
2017-03-26 20:34:42 -0500

    emulate the fivethirtyeight graph and also plot sugars vs. energy for a sample of 25 foods

--- connect.R
+++ connect.R
@@ -64,13 +64,45 @@ ggplot(data = na.omit(all_foods), aes(x = ndbno, y = value, colour = nutrient)) 
   theme_light()
 
 
+# take a sample of just 100 of the most common 1000 foods
 some_foods <- sample_n(all_foods, 100)
 
-ggplot(data = na.omit(some_foods[some_foods[["nutrient"]] == "Energy", ]), aes(x = name, y = value)) +
+# trim the names
+some_foods$name <- strtrim(some_foods$name, 20)
+
+ggplot(data = na.omit(some_foods[some_foods[["nutrient"]] == "Energy", ]), aes(x = ndbno, y = value)) +
   geom_point() +
-  theme_light()
+  geom_text_repel(aes(label = name), 
+                  box.padding = unit(1, "lines"),
+                  family = "Helvetica",
+                  size = 3.5,
+                  label.size = 0.5) +
+  theme_bw() +
+  theme(panel.background=element_rect(fill="#F0F0F0")) +
+  theme(plot.background=element_rect(fill="#F0F0F0")) +
+  theme(panel.border=element_rect(colour="#F0F0F0")) +
+  theme(panel.grid.major=element_line(colour="#D0D0D0",size=.75)) +
+  theme(axis.ticks=element_blank()) +
+  ggtitle("Energy of Common Foods") +
+  theme(plot.title=element_text(face="bold",colour="#3C3C3C",size=20)) +
+  ylab("Kcal/100g") +
+  xlab("NDB number") +
+  theme(axis.text.x=element_text(size=11,colour="#535353",face="bold")) +
+  theme(axis.text.y=element_text(size=11,colour="#535353",face="bold")) +
+  theme(axis.title.y=element_text(size=11,colour="#535353",face="bold")) +
+  theme(axis.title.x=element_text(size=11,colour="#535353",face="bold"))
 
 
 
+
+ggplot(data = some_foods[some_foods[["nutrient"]] == "Energy", ], aes(x = some_foods[some_foods[["nutrient"]] == "Energy", ]$value, 
+                                       y = some_foods[some_foods[["nutrient"]] == "Sugars, total", ]$value,
+                                       colour = factor(ndbno))) +
+  geom_point() +
+  geom_text_repel(aes(label = name), 
+                  box.padding = unit(0.45, "lines"),
+                  family = "Courier",
+                  label.size = 0.5) +
+  theme_bw()
 
 
