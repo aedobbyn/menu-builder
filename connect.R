5b6dbd99eb9e7ce9a335c998a2c0d8d4035a3288
aedobbyn <adobbs11@gmail.com>
2017-03-26 20:44:49 -0500

    add a graph that does the same thing with bars instead

--- connect.R
+++ connect.R
@@ -67,9 +67,11 @@ ggplot(data = na.omit(all_foods), aes(x = ndbno, y = value, colour = nutrient)) 
 # take a sample of just 100 of the most common 1000 foods
 some_foods <- sample_n(all_foods, 100)
 
-# trim the names
+# trim the names to 20 characters
 some_foods$name <- strtrim(some_foods$name, 20)
 
+# ---- fivethirtyeight rip off graph -----
+# plot energy per 100g for a sample of
 ggplot(data = na.omit(some_foods[some_foods[["nutrient"]] == "Energy", ]), aes(x = ndbno, y = value)) +
   geom_point() +
   geom_text_repel(aes(label = name), 
@@ -92,9 +94,28 @@ ggplot(data = na.omit(some_foods[some_foods[["nutrient"]] == "Energy", ]), aes(x
   theme(axis.title.y=element_text(size=11,colour="#535353",face="bold")) +
   theme(axis.title.x=element_text(size=11,colour="#535353",face="bold"))
 
+# same thing except bar graph
+ggplot(data = na.omit(some_foods[some_foods[["nutrient"]] == "Energy", ]), aes(x = name, y = value)) +
+  geom_bar(stat = "identity") +
+  theme_bw() +
+  theme(panel.background=element_rect(fill="#F0F0F0")) +
+  theme(plot.background=element_rect(fill="#F0F0F0")) +
+  theme(panel.border=element_rect(colour="#F0F0F0")) +
+  theme(panel.grid.major=element_line(colour="#D0D0D0",size=.75)) +
+  theme(axis.ticks=element_blank()) +
+  ggtitle("Energy of Common Foods") +
+  theme(plot.title=element_text(face="bold",colour="#3C3C3C",size=20)) +
+  ylab("Kcal/100g") +
+  xlab("NDB number") +
+  theme(axis.text.x=element_text(size=11,colour="#535353",face="bold")) +
+  theme(axis.text.y=element_text(size=11,colour="#535353",face="bold")) +
+  theme(axis.title.y=element_text(size=11,colour="#535353",face="bold")) +
+  theme(axis.title.x=element_text(size=11,colour="#535353",face="bold")) +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1, family = "Helvetica"))
 
 
 
+# plot energy vs. sugar
 ggplot(data = some_foods[some_foods[["nutrient"]] == "Energy", ], aes(x = some_foods[some_foods[["nutrient"]] == "Energy", ]$value, 
                                        y = some_foods[some_foods[["nutrient"]] == "Sugars, total", ]$value,
                                        colour = factor(ndbno))) +
