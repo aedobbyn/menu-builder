28626e167197a5b70b8d6ff694ba04afa918dab9
aedobbyn <adobbs11@gmail.com>
2017-04-12 15:42:09 -0500

    successful funciton to send 3 requests and rbind together

--- send_requests.R
+++ send_requests.R
@@ -3,25 +3,26 @@
 # https://api.nal.usda.gov/ndb/list?format=json&lt=f&sort=n&api_key=DEMO_KEY
 
 
+# at some point will want to figure out how to keep sending requests until we have every row
+# while loop maybe: find out how to get total somehow and while raw_req$list$item$offset < total, keep sending requests
 get_food_list <- function(n_req, offset) {
   df <- NULL
   offset <- 0
   n_req <- 3
-  for (i in n_req) {
-    raw_req <- fromJSON(paste0("https://api.nal.usda.gov/ndb/list?format=json&lt=f&sort=y&offset=", offset, 
+  for (i in 1:n_req) {
+    print(i)
+    raw_req <- fromJSON(paste0("https://api.nal.usda.gov/ndb/list?format=json&lt=f&sort=n&offset=", offset,
                                "&api_key=", 
-                               key),
-                        flatten = TRUE)
+                                            key),
+                                     flatten = TRUE)
+      print(offset)
     this_food_list <- as_tibble(raw_req$list$item)
     df <- rbind(df, this_food_list)
     offset <- offset + nrow(this_food_list)
   }
-  df <- as_tibble(df)
+  df
 }
 
-get_food_list()
+fl <- get_food_list()
 
 
-food_list <- fromJSON(paste0("https://api.nal.usda.gov/ndb/list?format=json&lt=f&sort=n&api_key=", 
-                          key),
-                   flatten = TRUE)
