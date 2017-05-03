fe77a71891115319c3c9ac6c89381791710c6f9e
aedobbyn <adobbs11@gmail.com>
2017-05-03 14:34:05 -0500

    combine the positive and must_restrict scores

--- score_menu.R
+++ score_menu.R
@@ -3,6 +3,8 @@
 
 # no extra credit for going above the daily min. best score is 0, worst score is negative infinity.
 # max score is 0, min score is -infinity
+
+# a positive 
 pos_score <- function(orig_menu) {
   total_nut_score <- 0
   
@@ -10,7 +12,7 @@ pos_score <- function(orig_menu) {
     nut_considering <- pos_df$positive_nut[p]    # grab the name of the nutrient we're examining
     val_nut_considering <- (sum(orig_menu[[nut_considering]] * orig_menu$GmWt_1))/100   # get the total amount of that nutrient in our original menu
     
-    nut_score <- val_nut_considering - pos_df$value[p]   # amount it is - min amount it's supposed to be
+    nut_score <- (-1)*(pos_df$value[p] - val_nut_considering)    # (-1)*(min amount it's supposed to be - amount it is here)
     
     if (nut_score > 0) {
       nut_score <- 0
@@ -25,7 +27,10 @@ pos_score(master_menu)
 
 
 # we both penalize for going over the max and give you brownie points for getting below the max
-# more positive score is better
+# more positive score is better (same directionality of goodness as pos_score)
+
+# a negative score on any one nutrient means you're over the max value you should be: bad
+# a negative score means you're below the max: good job
 mr_score <- function(orig_menu) {
   total_mr_score <- 0
   
@@ -33,11 +38,9 @@ mr_score <- function(orig_menu) {
     mr_considering <- mr_df$must_restrict[m]    # grab the name of the nutrient we're examining
     val_mr_considering <- (sum(orig_menu[[mr_considering]] * orig_menu$GmWt_1))/100   # get the total amount of that nutrient in our original menu
     
-    mr_score <- pos_df$value[m] - val_mr_considering   # max amount it's supposed to be - amount it is
-    print(paste0('this score: ', mr_score))
-    
+    mr_score <- (pos_df$value[m] - val_mr_considering)  # max amount it's supposed to be - amount it is
+
     total_mr_score <- total_mr_score + mr_score
-    print(paste0('running total: ', total_mr_score))
     total_mr_score
     
   }
@@ -48,6 +51,13 @@ mr_score(menu)
 mr_score(master_menu)
 
 
+# combine the two scores
+score_menu <- function(orig_menu) {
+  unhealthiness_score <- pos_score(orig_menu) + mr_score(orig_menu)
+  unhealthiness_score
+}
 
+score_menu(menu)
+score_menu(master_menu)
 
 
