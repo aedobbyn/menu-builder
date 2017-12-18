c6344acb3200833d4b80bd565faf8262bdc3b07d
aedobbyn <adobbs11@gmail.com>
2017-12-17 19:13:11 -0600

    some progress using Rglpk_solve_LP

--- scripts/solve.R
+++ scripts/solve.R
@@ -12,14 +12,32 @@ mr_df_small <- mr_df[1:3, ]
 # Constraints on all nutrients, must_restrics, and on calories
 # Vary GmWt_1
 # Optimize cost
-lp <- make.lp((nrow(pos_df_small) + nrow(mr_df_small) + 1), 1)
+# lp <- make.lp((nrow(pos_df_small) + nrow(mr_df_small) + 1), 2)
+lp <- make.lp(nrow(pos_df_small), 2)
 
-# Keep only columns we care about
+# -- Set dimnames --
+# Constraints in rows
+# row_names <- c(pos_df_small$positive_nut, mr_df$must_restrict, "Calories")
+row_names <- c(pos_df_small$positive_nut)
+col_names <- c("Nutrient", "GmWt_1", "Cost")
+
+dimnames(lp) <- list(row_names, col_names)
+
+
+# Simplify our menu space
+
 cols_to_keep <- c(pos_df_small$positive_nut, mr_df$must_restrict, "Shrt_Desc", "Energ_Kcal", "GmWt_1")
-menu_small <- menu[, which(names(menu) %in% cols_to_keep)]
+menu_small <- menu[, which(names(menu) %in% cols_to_keep)] %>% 
+  slice(1:3) %>% 
+  mutate(
+    shorter_desc = map_chr(Shrt_Desc, grab_first_word, splitter = ","), # Take only the fist word
+    cost = runif(nrow(.), min = 1, max = 10) %>% round(digits = 2) # Add a cost column
+  ) %>% 
+  select(shorter_desc, GmWt_1, cost, everything(), -Shrt_Desc)
 
-# Add a cost column
-menu_small$cost <- runif(nrow(menu_small), min = 1, max = 10) %>% round(digits = 2)
+
+# 
+
 
 
 # ---- Minimize cost
@@ -42,16 +60,18 @@ menu_small$cost <- runif(nrow(menu_small), min = 1, max = 10) %>% round(digits =
 # Iron_mg * GmWt_1 > 18
 # Magnesium_mg * GmWt_1 > 400
 
+# First column
 set.column(lp, 1, c(1, 1, 1000))
+# Second column
 set.column(lp, 2, c(1, 1, 0))
+# Third column
 set.column(lp, 3, c(1, 1, 0))
 set.rhs(lp, c(1000, 18, 400))
+set.constr.type(my.lp, rep(">", 3))   # third column
+set.objfn(my.lp, c(0, 1, 1))  # minimize product of GmWt_1 (column 2) and Cost (column 3)
 
 
-row_names <- c("Nutrient", "GmWt_1")
-col_names <- c(pos_df_small$positive_nut, mr_df$must_restrict, "Calories")
 
-dimnames(lp) <- list(RowNames, ColNames)
 
 
 
@@ -67,8 +87,34 @@ dimnames(lp) <- list(RowNames, ColNames)
   ## x_1, x_2, x_3 are non-negative real numbers
 library(Rglpk)
 obj <- c(2, 4, 3)
-mat <- matrix(c(3, 2, 1, 4, 1, 3, 2, 2, 2), nrow = 3)
+mat <- matrix(c(3, 2, 1, 
+                4, 1, 3, 
+                2, 2, 2), nrow = 3)
 dir <- c("<=", "<=", "<=")
 rhs <- c(60, 40, 80)
 max <- TRUE
 Rglpk_solve_LP(obj, mat, dir, rhs, max = max)
+
+# minimize: cherries::cost*cherries::GmWt_1 + cheese::cost*cheese::GmWt_1 + cereals::cost*cereals::GmWt_1
+
+# subject to: 
+  
+cherries::GmWt_1*Calcium_Mg + cheese::GmWt_1*Iron_mg + cereals::GmWt_1*Magnesium_mg > 1000
+
+obj_fn <- c(2.29, 2.62, 3.88)
+
+                  # Calcium,  Iron,   Magnesium
+menu_mat <-   matrix(c(11,    1253,   307,            # CHERRIES
+                       0.36,  0.87,   11.30,          # CHEESE
+                       9,     51,     84,             # CEREALS
+                       46,    415,    379),           # calories
+                     nrow = 4)  
+dir <- c(">", ">", ">", ">")
+rhs <- c(1000, 18, 400, 300)    # CA, FE, MG, calories
+Rglpk_solve_LP(obj_fn, mat, dir, rhs, max = FALSE)
+
+
+
+
+
+
