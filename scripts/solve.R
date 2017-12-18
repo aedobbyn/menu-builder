813a6135a8ea97f62167c281f7cefc77d8c05f59
aedobbyn <adobbs11@gmail.com>
2017-12-17 20:15:18 -0600

    clean up solve.R

--- scripts/solve.R
+++ scripts/solve.R
@@ -1,31 +1,20 @@
 
-# Solver
+# LP Solver
 # https://r-forge.r-project.org/scm/viewvc.php/*checkout*/pkg/inst/doc/lpSolveAPI.pdf?revision=88&root=lpsolve
+
+# GNU solver
+# https://cran.r-project.org/web/packages/Rglpk/Rglpk.pdf
 
 source("./scripts/menu_builder.R")
 
 library(lpSolveAPI)
+library(Rglpk)
 
-pos_df_small <- pos_df[1:3, ]
-mr_df_small <- mr_df[1:3, ]
-
-# Constraints on all nutrients, must_restrics, and on calories
-# Vary GmWt_1
-# Optimize cost
-# lp <- make.lp((nrow(pos_df_small) + nrow(mr_df_small) + 1), 2)
-lp <- make.lp(nrow(pos_df_small), 2)
-
-# -- Set dimnames --
-# Constraints in rows
-# row_names <- c(pos_df_small$positive_nut, mr_df$must_restrict, "Calories")
-row_names <- c(pos_df_small$positive_nut)
-col_names <- c("Nutrient", "GmWt_1", "Cost")
-
-dimnames(lp) <- list(row_names, col_names)
-
+# Pick a few nutrients to start with
+pos_df_small <- pos_df[1:3, ] %>% rename(nutrient = positive_nut)
+mr_df_small <- mr_df[1:3, ] %>% rename(nutrient = must_restrict)
 
 # Simplify our menu space
-
 cols_to_keep <- c(pos_df_small$positive_nut, mr_df$must_restrict, "Shrt_Desc", "Energ_Kcal", "GmWt_1")
 menu_small <- menu[, which(names(menu) %in% cols_to_keep)] %>% 
   slice(1:3) %>% 
@@ -36,90 +25,74 @@ menu_small <- menu[, which(names(menu) %in% cols_to_keep)] %>%
   select(shorter_desc, GmWt_1, cost, everything(), -Shrt_Desc)
 
 
-# 
+# -------  Rglpk example  -------
+# One row per constraint
+## maximize:   2 x_1 + 4 x_2 + 3 x_3
+## subject to: 3 x_1 + 4 x_2 + 2 x_3 <= 60
+##             2 x_1 + 1 x_2 + 2 x_3 <= 40
+##             1 x_1 + 3 x_2 + 2 x_3 <= 80
+obj_ex <- c(2, 4, 3)
+mat_ex <- matrix(c(3, 4, 2, 
+                   2, 1, 2, 
+                   1, 3, 2), nrow = 3, byrow = TRUE)
+dir_ex <- c("<=", "<=", "<=")
+rhs_ex <- c(60, 40, 80)
+max_ex <- TRUE
+Rglpk_solve_LP(obj_ex, mat_ex, dir_ex, rhs_ex, max = max_ex)
 
 
-
-# ---- Minimize cost
-# cost = cost * GmWt_1
-
-# ----- Constraints
-# # Example
-# subject to: x1 + 3x2 ≤ 4
-#             x1 + x2 ≤ 2
-#             2x1 + ≤ 3
-# set.column(lp, 1, c(1, 1, 2))
-# set.column(lp, 2, c(3, 1, 0))
-# set.rhs(lp, c(4, 2, 3))
-
-
-# calories = Energ_Kcal * GmWt_1
-
-# ----- Set constraints
-# Calcium_mg * GmWt_1 > 1000
-# Iron_mg * GmWt_1 > 18
-# Magnesium_mg * GmWt_1 > 400
-
-# First column
-set.column(lp, 1, c(1, 1, 1000))
-# Second column
-set.column(lp, 2, c(1, 1, 0))
-# Third column
-set.column(lp, 3, c(1, 1, 0))
-set.rhs(lp, c(1000, 18, 400))
-set.constr.type(my.lp, rep(">", 3))   # third column
-set.objfn(my.lp, c(0, 1, 1))  # minimize product of GmWt_1 (column 2) and Cost (column 3)
-
-
-
-
-
-
-  
-  
-  
-  
-  ## Simple linear program.
-  ## maximize: 2 x_1 + 4 x_2 + 3 x_3
-  ## subject to: 3 x_1 + 4 x_2 + 2 x_3 <= 60
-  ## 2 x_1 + x_2 + 2 x_3 <= 40
-  ## x_1 + 3 x_2 + 2 x_3 <= 80
-  ## x_1, x_2, x_3 are non-negative real numbers
-library(Rglpk)
-obj <- c(2, 4, 3)
-mat <- matrix(c(3, 2, 1, 
-                4, 1, 3, 
-                2, 2, 2), nrow = 3)
-dir <- c("<=", "<=", "<=")
-rhs <- c(60, 40, 80)
-max <- TRUE
-Rglpk_solve_LP(obj, mat, dir, rhs, max = max)
+# ---- Small example ----
 
 # minimize: cherries::cost*cherries::GmWt_1 + cheese::cost*cheese::GmWt_1 + cereals::cost*cereals::GmWt_1
-
 # subject to: 
-  
-cherries::GmWt_1*Calcium_Mg + cheese::GmWt_1*Iron_mg + cereals::GmWt_1*Magnesium_mg > 1000
+# cherries::GmWt_1*Calcium_Mg + cheese::GmWt_1*Iron_mg + cereals::GmWt_1*Magnesium_mg > 1000, etc.
 
 obj_fn <- c(2.29, 2.62, 3.88)
 
                   # Calcium,  Iron,   Magnesium
-menu_mat <-   matrix(c(11,    1253,   307,            # CHERRIES
-                       0.36,  0.87,   11.30,          # CHEESE
-                       9,     51,     84),             # CEREALS
-                       # 46,    415,    379),           # calories
-                     nrow = 3)  
+menu_mat <-   matrix(c(11,    0.36,   9,            # CHERRIES
+                       1253,  0.87,   51,          # CHEESE
+                       307,   11.30,  84),             # CEREALS
+                     nrow = 3, byrow = TRUE)  
 dir <- c(">", ">", ">")
-rhs <- c(1000, 18, 400)    # CA, FE, MG, calories
-Rglpk_solve_LP(obj_fn, mat, dir, rhs, max = FALSE)
+rhs <- c(1000, 18, 400)    # Ca, Fe, Mg
+solution <- Rglpk_solve_LP(obj_fn, menu_mat, dir, rhs, max = FALSE)
 
 
+# --- Programmatically ---
+solve_it <- function(df, nutrient_df, maximize = FALSE) {
+  n_foods <- nrow(df)
 
-# Programmatically
+  construct_matrix <- function(df, nutrient_df) {
+    mat_base <- NULL
+    for (i in 1:n_foods) {
+      mat_base <- c(mat_base, df[i, ][nutrient_df[["nutrient"]]] %>% as_vector())
+    }
+    mat <- matrix(mat_base, nrow = n_foods, byrow = TRUE)
+    return(mat)
+  }
+  
+  mat <- construct_matrix(df, nutrient_df)
+  message("Matrix below:")
+  print(mat)
+  
+  dir <- rep(">", n_foods)
+  rhs <- nutrient_df[["value"]]
+  obj_fn <- df[["cost"]]
+  
+  out <- Rglpk_solve_LP(obj_fn, mat, dir, rhs, max = maximize)
+  return(out)
+}
+
+solution_out <- solve_it(menu_small, pos_df_small)
+
+
+# Test
+assertthat::are_equal(solution$optimum, solution_out$optimum)
+
 
 construct_matrix <- function(df) {
   mat_base <- NULL
-  n_foods <<- nrow(df)
   for (i in 1:n_foods) {
     mat_base <- c(mat_base, df[i, pos_df_small$positive_nut])
     mat <- matrix(mat_base, nrow = n_foods)
@@ -127,10 +100,14 @@ construct_matrix <- function(df) {
   return(mat)
 }
 
-mat <- construct_matrix(menu_small)
+mat2 <- construct_matrix(menu_small)
 
-dir <- rep(">", n_foods)
+dir2 <- rep(">", n_foods)
 
-rhs <- pos_df_small$value
+rhs2 <- pos_df_small$value
+
+obj_fn2 <- menu_small[["cost"]]
+
+out2 <- Rglpk_solve_LP(obj_fn2, mat2, dir2, rhs2, max = FALSE)
 
 
