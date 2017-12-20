fcd8619d93dc54193c39a882eb709a46f588c085
aedobbyn <adobbs11@gmail.com>
2017-12-20 05:38:31 -0600

    start to add in MRs to the equation. changing menu matrix from having number of rows in food to number of constraints. also cbinding solution col to original menu df

--- scripts/solve.R
+++ scripts/solve.R
@@ -7,12 +7,10 @@
 
 source("./scripts/menu_builder.R")
 
+library(feather)
 library(lpSolveAPI)
 library(Rglpk)
 
-# Pick a few nutrients to start with
-pos_df_small <- pos_df[1:3, ] %>% rename(nutrient = positive_nut)
-mr_df_small <- mr_df[1:3, ] %>% rename(nutrient = must_restrict)
 
 # Simplify our menu space
 cols_to_keep <- c(pos_df_small$positive_nut, mr_df$must_restrict, "Shrt_Desc", "Energ_Kcal", "GmWt_1")
@@ -24,6 +22,19 @@ menu_small <- menu[, which(names(menu) %in% cols_to_keep)] %>%
   ) %>% 
   select(shorter_desc, GmWt_1, cost, everything(), -Shrt_Desc)
 
+# OR, read in
+menu_small <- read_feather(menu_small, "./data/menu_small.feather")
+
+
+
+# Pick a few nutrients to start with and give them a flag for whether they're a must-restrict or not
+pos_df_small <- pos_df[1:3, ] %>% rename(nutrient = positive_nut) %>% 
+  mutate(is_mr = FALSE)
+mr_df_small <- mr_df[1:3, ] %>% rename(nutrient = must_restrict) %>% 
+  mutate(is_mr = TRUE)
+nut_df_small <- bind_rows(pos_df_small, mr_df_small)
+
+
 
 
 # ---- Small example ----
@@ -34,14 +45,24 @@ menu_small <- menu[, which(names(menu) %in% cols_to_keep)] %>%
 
 obj_fn <- c(2.29, 2.62, 3.88)
 
-                  # Calcium,  Iron,   Magnesium
-menu_mat <-   matrix(c(11,    0.36,   9,            # CHERRIES
-                       1253,  0.87,   51,           # CHEESE
-                       307,   11.30,  84),          # CEREALS
-                     nrow = 3, byrow = TRUE)  
-dir <- c(">", ">", ">")
-rhs <- c(1000, 18, 400)    # Ca, Fe, Mg
+                     # Ca,     Fe,    Mg,   Lipid,   Na,   Chol
+menu_mat <-   matrix(c(11,    0.36,   9,     0.13,    1,    0,             # CHERRIES
+                       1253,  0.87,   51,    27.34,   1696, 72,            # CHEESE
+                       307,   11.30,  84,     6.37,   499,  0 ),           # CEREALS
+                     nrow = 6, byrow = TRUE)  
+dir <- c(">", ">", ">", "<", "<", "<")
+rhs <- c(1000, 18, 400, 65, 2400, 300)    # Ca, Fe, Mg, Lipid, Na, Chol
 solution <- Rglpk_solve_LP(obj_fn, menu_mat, dir, rhs, max = FALSE)
+
+# Cost is solution$optimum
+
+# cbind the solved amounts to the original menu
+solved_col <- list(solution_amounts = solution$solution) %>% as_tibble()
+menu_small_solved <- menu_small %>% bind_cols(solved_col) %>% 
+  select(shorter_desc, solution_amounts, everything())
+
+# What's the max solution amount? If too high, may need to adjust down 
+max_solution_amount <- solution$solution[which(solution$solution == max(solution$solution))]
 
 
 # --- Programmatically ---
@@ -53,7 +74,7 @@ solve_it <- function(df, nutrient_df, maximize = FALSE) {
     for (i in 1:n_foods) {
       mat_base <- c(mat_base, df[i, ][nutrient_df[["nutrient"]]] %>% as_vector())
     }
-    mat <- matrix(mat_base, nrow = n_foods, byrow = TRUE)
+    mat <- matrix(mat_base, nrow = nrow(nutrient_df), byrow = TRUE)
     return(mat)
   }
   
@@ -61,17 +82,39 @@ solve_it <- function(df, nutrient_df, maximize = FALSE) {
   message("Matrix below:")
   print(mat)
   
-  dir <- rep(">", n_foods)
+  dir_pos <- rep(">", nutrient_df %>% filter(is_mr == FALSE) %>% ungroup() %>% count() %>% as_vector())
+  dir_mr <- rep("<", nutrient_df %>% filter(is_mr == TRUE) %>% ungroup() %>% count() %>% as_vector())
+  
+  dir <- c(dir_pos, dir_mr)
   rhs <- nutrient_df[["value"]]
   obj_fn <- df[["cost"]]
   
   out <- Rglpk_solve_LP(obj_fn, mat, dir, rhs, max = maximize)
+  message(paste0("Cost is ", out$optimum))
+  
   return(out)
 }
 
 solution_out <- solve_it(menu_small, pos_df_small)
 
 
-# Test
-assertthat::are_equal(solution$optimum, solution_out$optimum)
+# # Test
+# assertthat::are_equal(solution$optimum, solution_out$optimum)
+
+
+
+# Take a menu and return a solved menu
+solve_menu <- function(df, nutrient_df) {
+  sol <- df %>% solve_it(nutrient_df)
+  
+  df_solved <- df %>% bind_cols(solved_col) %>% 
+    select(shorter_desc, solution_amounts, everything())
+  
+  max_food <- df_solved %>% filter(solution_amounts == max(solution_amounts))   # modify for if we've got mult maxes
+  
+  message(paste0("We've got a lot of ", max_food$shorter_desc), ". ", 
+          solution_amounts, " of it.")
+  
+  return(df_solved)
+}
 
