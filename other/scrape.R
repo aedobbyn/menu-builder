e129f318a6155e98e3340a7126ad205a7b8389a2
aedobbyn <adobbs11@gmail.com>
2017-12-31 05:58:39 -0500

    add the recipe name to our list of dataframes and simplify recipe function jobs

--- other/scrape.R
+++ other/scrape.R
@@ -2,16 +2,17 @@ library(tidyverse)
 library(rvest)
 
 example_url <- "http://allrecipes.com/recipe/244950/baked-chicken-schnitzel/?internalSource=streams&referringId=1947&referringContentType=recipe%20hub&clickId=st_trending_b"
+schnitzel <- example_url %>% get_recipes()
+example_url %>% get_recipe_name()
 
 base_url <- "http://allrecipes.com/recipe/"
+urls <- grab_urls(base_url, 244940:244950)
 
 grab_urls <- function(base_url, id) {
   id <- as.character(id)
   recipe_url <- str_c(base_url, id)
   return(recipe_url)
 }
-
-urls <- grab_urls(base_url, 244940:244950)
 
 remove_whitespace <- function(str) {
   str <- str %>% str_split(" ") %>% as_vector()
@@ -20,27 +21,40 @@ remove_whitespace <- function(str) {
   return(str)
 }
 
-get_recipes <- function(url) {
-  
-  recipe_page <- read_html(url)
-  
-  recipe <- recipe_page %>% 
+# Takes a page (a url that's been read_html'd)
+get_recipe_name <- function(page) {
+  recipe_name <- page %>% 
+    html_nodes(".recipe-summary__h1") %>% 
+    html_text() 
+  return(recipe_name)
+}
+
+get_recipe_content <- function(page) {
+  recipe <- page %>% 
     html_nodes(".checkList__line") %>% 
     html_text() %>% 
     str_replace_all("ADVERTISEMENT", "") %>% 
     str_replace_all("\n", "") %>% 
     str_replace_all("\r", "") %>% 
     str_replace_all("Add all ingredients to list", "")
-  
-  recipe <- recipe %>% 
-    map(remove_whitespace) %>% as_vector()
-  
-  recipe
+  return(recipe)
 }
 
-urls[1:3] %>% map(get_recipes)
+get_recipes <- function(url) {
+  
+  recipe_page <- read_html(url)
+  
+  recipe <- recipe_page %>% 
+    get_recipe_content() %>% 
+    map(remove_whitespace) %>% as_vector()
+  
+  recipe_name <- get_recipe_name(recipe_page)
+  
+  recipe_df <- list(this_name = recipe) %>% as_tibble()   # could do with deparse(recipe_name)?
+  names(recipe_df) <- recipe_name
+  
+  return(recipe_df)
+}
 
-
-
-
+some_recipes <- urls[1:3] %>% map(get_recipes)
 
