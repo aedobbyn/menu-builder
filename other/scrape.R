c846b830a51770f86230362f60eb37cfea333cde
aedobbyn <adobbs11@gmail.com>
2017-12-31 05:29:23 -0500

    write function for getting a bunch of urls and grabbing recipes from them by id

--- other/scrape.R
+++ other/scrape.R
@@ -1,19 +1,17 @@
-
+library(tidyverse)
 library(rvest)
 
+example_url <- "http://allrecipes.com/recipe/244950/baked-chicken-schnitzel/?internalSource=streams&referringId=1947&referringContentType=recipe%20hub&clickId=st_trending_b"
 
-recipe_url <- "http://allrecipes.com/recipe/244950/baked-chicken-schnitzel/?internalSource=streams&referringId=1947&referringContentType=recipe%20hub&clickId=st_trending_b"
+base_url <- "http://allrecipes.com/recipe/"
 
-recipe_page <- read_html(recipe_url)
+grab_urls <- function(base_url, id) {
+  id <- as.character(id)
+  recipe_url <- str_c(base_url, id)
+  return(recipe_url)
+}
 
-recipe <- recipe_page %>% 
-  html_nodes(".checkList__line") %>% 
-  html_text() %>% 
-  str_replace_all("ADVERTISEMENT", "") %>% 
-  str_replace_all("\n", "") %>% 
-  str_replace_all("\r", "") %>% 
-  str_replace_all("Add all ingredients to list", "")
-
+urls <- grab_urls(base_url, 244940:244950)
 
 remove_whitespace <- function(str) {
   str <- str %>% str_split(" ") %>% as_vector()
@@ -22,5 +20,27 @@ remove_whitespace <- function(str) {
   return(str)
 }
 
-recipe <- recipe %>% 
-  map(remove_whitespace) %>% as_vector()
+get_recipes <- function(url) {
+  
+  recipe_page <- read_html(url)
+  
+  recipe <- recipe_page %>% 
+    html_nodes(".checkList__line") %>% 
+    html_text() %>% 
+    str_replace_all("ADVERTISEMENT", "") %>% 
+    str_replace_all("\n", "") %>% 
+    str_replace_all("\r", "") %>% 
+    str_replace_all("Add all ingredients to list", "")
+  
+  recipe <- recipe %>% 
+    map(remove_whitespace) %>% as_vector()
+  
+  recipe
+}
+
+urls[1:3] %>% map(get_recipes)
+
+
+
+
+
