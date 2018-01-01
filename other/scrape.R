fa0574615a8279f3f9227a3520a8707970028e0e
aedobbyn <adobbs11@gmail.com>
2018-01-01 13:23:51 -0500

    multiply portions

--- other/scrape.R
+++ other/scrape.R
@@ -88,7 +88,7 @@ more_urls <- grab_urls(base_url, 10000:15000)
 more_recipes <- more_urls %>% map(get_recipes)
 
 
-
+# Take our list of recipes and make them into a dataframe with 
 dfize <- function(lst) {
   df <- NULL
   lst <- lst[!lst == "Bad URL"]
@@ -114,27 +114,62 @@ measures_collapsed <- str_c(measures, collapse = "|")
 # Match any number, even if it has a decimal or slash in it
 portions_reg <- "[[:digit:]]+\\.*[[:digit:]]*+\\/*[[:digit:]]*"
 
+# Turn fractions into decimals but keep them as character so we can put this in pipeline before 
+# the as.numeric() call
 frac_to_dec <- function(e) {
-  e <- parse(text = e) %>% eval() %>% as.character()
-  return(e)
+  out <- parse(text = e) %>% eval() %>% as.character()
+  return(out)
+}
+
+map_frac_to_dec <- function(e) {
+  out <- NULL
+  # browser()
+  for (i in e) {
+    out <- e %>% map_chr(frac_to_dec)
+  }
+  return(out)
+}
+
+# Multiply numbers by each other
+# e.g., if we've got 2 (14 ounce) cans beef broth we want to know we need 28 oz
+multiply_portions <- function(e) {
+  out <- e %>% map_chr(frac_to_dec) %>% as.numeric() 
+  if (length(e) > 1) {
+    out <- out %>% reduce(`*`)
+  }
+  return(out)
 }
 
 get_portions <- function(df) {
   df <- df %>% 
     mutate(
-      portion_num = str_extract(ingredients, portions_reg) %>% 
-        map_chr(frac_to_dec) %>% as.numeric() %>% round(digits = 2),
-      portion_name = str_extract(ingredients, measures_collapsed)
+      portion_num = str_extract_all(ingredients, portions_reg) %>% 
+        map(map_frac_to_dec) %>% as.numeric() %>% 
+        map_dbl(multiply_portions) %>% round(digits = 2),
+      portion_name = str_extract(ingredients, measures_collapsed) # %>% first()
     )
   return(df)
 }
 
 
 
-some_recipes_df_decimal <- some_recipes_df
-some_recipes_df_decimal[1, ] <- "1.2 ounces chipotle cooking sauce"
+some_recipes_tester <- some_recipes_df
+some_recipes_tester[1, ] <- "1.2 ounces of something with a decimal"
+some_recipes_tester[2, ] <- "3 (14 ounce) can reduced-sodium beef broth"
+some_recipes_tester <- some_recipes_tester %>% slice(1:3) %>% select(-recipe_name)
 
-get_portions(some_recipes_df_decimal) %>% 
-  select(-recipe_name)
+bar <- get_portions(some_recipes_tester) 
+bar
+  
+baz <- str_extract_all(some_recipes_tester$ingredients, portions_reg) 
+
+baz[[2]] %>% map_chr(frac_to_dec)
+
+foo <- str_extract_all(some_recipes_df_decimal$ingredients, portions_reg) 
+
+foo[[2]] %>% map_chr(frac_to_dec) %>% as.numeric() %>% 
+  reduce(`*`)
+
+foo %>% map_dbl(multiply_portions)
 
 
