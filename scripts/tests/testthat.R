29630e55b06735c8d26347c4c0cf5a3f5745b944
aedobbyn <adobbs11@gmail.com>
2018-01-11 22:56:04 -0600

    move all of testing stuff into dobtools

--- scripts/tests/testthat.R
+++ scripts/tests/testthat.R
@@ -1,5 +1,6 @@
 
-source("./helpers/import_scripts.R")
+devtools::install_github("aedobbyn/dobtools")
+library(dobtools)
 library(testthat)
 library(stringr)
 library(feather)
@@ -9,15 +10,8 @@ library(tidyverse)
 dirs <- c("prep", "build", "score", "scrape", "solve")
 paths <- str_c("./scripts/", dirs)
 
-
-# import_scripts(paths)
-
-for (p in paths) {
-  for (f in list.files(p, pattern = "*.R", ignore.case = TRUE)) {
-    source(str_c(p, "/", f))
-  }
-}
-
+# Import all scripts from all the dirs above
+dobtools::import_scripts(paths)
 
 
 #### Load tests ####
@@ -25,60 +19,13 @@ path <- "./scripts/tests/testthat/"
 test_types <- c("build", "scrape", "solve")
 test_files <- str_c(path, str_c("test_", test_types), ".R")
 # Add a fake file for testing purposes
-test_files <- c("./scripts/tests/testthat/test_fake.R", test_files)
+fake_test <- "./scripts/tests/testthat/test_fake.R"
+test_files <- c(fake_test, test_files)
 
+# Run a test on just our small fake file
+fake_test %>% dobtools::run_tests()
 
-# Return whether an individual test passed or not
-test_it <- function(f, verbose = FALSE) {
-  if (verbose == FALSE) {
-    result <- suppressMessages(suppressWarnings(try(source(f), silent = TRUE)))
-  } else {
-    result <- try(source(f), silent = TRUE)
-  }
-  
-  if (inherits(result, "try-error")) {
-    message(paste0(" --- ", f, " FAILED --- "))
-  } else {
-    message(paste0(" --- ", f, " PASSED --- "))
-  }
-}
-
-
-# Run individual tests interactively or not
-run_tests <- function(files = NULL, ext = NULL, line_by_line = TRUE) {
-  files <- str_c(files, ext)
-
-  if (line_by_line == FALSE ) {  # | interactive() == FALSE
-    for (i in seq_along(files)) {
-      test_it(files[i])
-    }
-    
-  } else {
-    i <- 1
-      while (i <= length(files)) {
-        answer <- readline(paste0("Should we test ", files[i], " ? \n y/n:       "))
-        
-        if (answer == "y" | answer == "Y") {
-          test_it(files[i])
-          i <- i + 1
-          
-        } else if (answer == "n" | answer == "N") {
-          message(paste0("Ok; not testing ", files[i], "."))
-          i <- i + 1
-          
-        } else if (answer == "q" | answer == "Q") {
-          message("Quitting tests.")
-          break
-          
-        } else {
-          message("Unrecognized choice submitted. Trying again.")
-          i <- i    # Step back one
-        }
-    }
-  } 
-}
-
-# Run interactively
+# Run all interactively
 test_files %>% run_tests()
 
 # Run silently and non-interactively
