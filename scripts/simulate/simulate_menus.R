f0aa332bda6bf9a9153ef275eeec82937879691c
aedobbyn <adobbs11@gmail.com>
2018-01-17 13:20:26 -0600

    fix the non-unique swapping of foods in by replacing filter(! NDB_No %in% scaled) in replace_food_w_better() with filter(! NDB_No %in% orig_menu)

--- scripts/simulate/simulate_menus.R
+++ scripts/simulate/simulate_menus.R
@@ -56,11 +56,23 @@ simulate_spectrum <- function(n_intervals = 10, n_sims = 2, from = -1, to = 1,
 # --- Summarise a spectrum ---
 summarise_status_spectrum <- function(spec) {
   
-  spec_summary <- spec %>% 
-    group_by(min_amount) %>% 
-    summarise(
-      sol_prop = mean(status)
-    )
+  # If this was a product of simulate_spectrum()
+  if (!"n_swaps_done" %in% names(spec)){
+    spec_summary <- spec %>% 
+      group_by(min_amount) %>% 
+      summarise(
+        sol_prop = mean(status)
+      )
+  
+  # If this was a product of simulate_swap_spectrum()
+  } else {
+    spec_summary <- spec %>% 
+      group_by(min_amount) %>% 
+      summarise(
+        sol_prop = mean(status),
+        mean_n_swaps_done = mean(n_swaps_done)
+      )
+  }
   
   return(spec_summary)
 }
@@ -75,8 +87,8 @@ summarise_status_spectrum <- function(spec) {
 
 
 
-
-simulate_swaps <- function(seed = NULL, min_food_amount = 0.5, n_swaps = 3, return_status = TRUE,
+# Analogous to get_status; we build a random menu and see how many swaps it takes to solve it, staying under the max number of swaps
+simulate_swaps <- function(seed = NULL, min_food_amount = 0.5, max_n_swaps = 3, return_status = TRUE,
                            verbose = TRUE) {  
   counter <- 0
   this_solution <- build_menu(seed = seed) %>% do_menu_mutates() %>% 
@@ -86,7 +98,7 @@ simulate_swaps <- function(seed = NULL, min_food_amount = 0.5, n_swaps = 3, retu
   
   this_menu <- this_solution %>% solve_menu()
   
-  while (counter < n_swaps & this_status == 1) {
+  while (counter < max_n_swaps & this_status == 1) {
     this_solution <- this_menu %>% do_single_swap() %>% 
       solve_it(min_food_amount = min_food_amount, verbose = verbose, only_full_servings = FALSE)
     this_status <- this_solution %>% purrr::pluck("status")
@@ -112,9 +124,12 @@ simulate_swaps(min_food_amount = 1)
 
 
 
-
-simulate_swap_spectrum <- function(n_intervals = 10, n_sims = 2, n_swaps = 3, from = -1, to = 1,
-                              min_food_amount = NULL, seed = NULL, verbose = FALSE) {
+# from is the lowest minimum portion size and to is the highest minimum portion size
+# We split the range from:to into n_intervals and do n_sims at each interval
+# For each simulation, we build a menu, try to solve it, and, if we can't, swap and solve as many times as we can until
+# we either solve it and record the number of swaps it took or hit our max_n_swaps and record the failure
+simulate_swap_spectrum <- function(n_intervals = 10, n_sims = 2, max_n_swaps = 3, from = -1, to = 1,
+                                   seed = NULL, verbose = FALSE) {
   
   interval <- (to - from) / n_intervals
   spectrum <- seq(from = from, to = to, by = interval) %>% rep(n_sims) %>% sort()
@@ -127,7 +142,7 @@ simulate_swap_spectrum <- function(n_intervals = 10, n_sims = 2, n_swaps = 3, fr
                        n_swaps_done = vector(length = length(spectrum)))
   
   for (i in seq_along(spectrum)) {
-    this_status_df <- simulate_swaps(seed = seeds[i], min_food_amount = spectrum[i], n_swaps = n_swaps, verbose = verbose)
+    this_status_df <- simulate_swaps(seed = seeds[i], min_food_amount = spectrum[i], max_n_swaps = max_n_swaps, verbose = verbose)
     if (!is.integer(this_status_df$status)) {
       this_status_df$status <- integer(0)     # If we don't get an integer value back, make it NA
     }
@@ -141,14 +156,30 @@ simulate_swap_spectrum <- function(n_intervals = 10, n_sims = 2, n_swaps = 3, fr
 
 simulate_swap_spectrum(n_intervals = 5, n_sims = 2, n_swaps = 3)
 
-half_sims <- simulate_swap_spectrum(n_intervals = 20, n_sims = 1, n_swaps = 3, seed = 9)
+simmed_swaps <- simulate_swap_spectrum(n_intervals = 20, n_sims = 5, n_swaps = 4, seed = 9)
 
-summarise_status_spectrum(half_sims)
+simmed_swaps_summary <- summarise_status_spectrum(simmed_swaps)
 
 ggplot() +
-  geom_smooth(data = half_sims, aes(min_amount, 1 - status, colour = factor(n_swaps_done)),
+  # geom_density_2d(data = simmed_swaps, aes(min_amount, 1 - status, colour = factor(n_swaps_done)),
+  #             se = FALSE) +
+  geom_smooth(data = simmed_swaps, aes(min_amount, 1 - status),
               se = FALSE) +
-  geom_point(data = half_sims, aes(min_amount, 1 - status)) +
+  geom_point(data = simmed_swaps_summary, aes(min_amount, 1 - sol_prop, colour = factor(mean_n_swaps_done))) +
+  # facet_wrap( ~ n_swaps_done) +
+  theme_minimal() +
+  ggtitle("Curve of portion size vs. solvability") +
+  labs(x = "Minimum portion size", y = "Proportion of solutions") +
+  ylim(0, 1) 
+
+
+
+ggplot() +
+  # geom_density_2d(data = simmed_swaps, aes(min_amount, 1 - status, colour = factor(n_swaps_done)),
+  #             se = FALSE) +
+  geom_smooth(data = simmed_swaps, aes(min_amount, n_swaps_done),
+              se = FALSE) +
+  # geom_point(data = simmed_swaps_summary, aes(min_amount, 1 - sol_prop, colour = factor(mean_n_swaps_done))) +
   # facet_wrap( ~ n_swaps_done) +
   theme_minimal() +
   ggtitle("Curve of portion size vs. solvability") +
