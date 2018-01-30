28c0b6941dd9724ec3b366b279d2640a64c9b1eb
aedobbyn <adobbs11@gmail.com>
2018-01-30 15:04:07 -0600

    flesh out solve_nutrients() and more simulating

--- scripts/solve/solve_it.R
+++ scripts/solve/solve_it.R
@@ -40,9 +40,9 @@ solve_it <- function(df, nut_df = nutrient_df, df_is_per_100g = TRUE, only_full_
   const_mat_names <- str_c(df$shorter_desc,  # Use combo of shorter_desc and NDB_No
         df$NDB_No, sep = ", ")  # so that names are interpretable but also unique
   
-  # # TODO: find a better fix than this to the non-uniqueness problem that probably comes from smart swapping
-  # rand_letters <- letters[sample(length(const_mat_names))]
-  # const_mat_names <- str_c(const_mat_names, rand_letters, sep = " - ")
+  # TODO: find a better fix than this to the non-uniqueness problem that probably comes from smart swapping
+  rand_letters <- letters[sample(length(const_mat_names))]
+  const_mat_names <- str_c(const_mat_names, rand_letters, sep = " - ")
   
   mat <- construct_matrix(df, nut_df)
   constraint_matrix <- mat %>% dplyr::as_data_frame() 
