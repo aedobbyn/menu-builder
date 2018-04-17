c5d7c9a6c1ba5a06e51457478e7522901abef715
aedobbyn <adobbs11@gmail.com>
2018-04-16 19:06:54 -0500

    rename units to ingredient_units

--- scripts/scrape/get_measurement_types.R
+++ scripts/scrape/get_measurement_types.R
@@ -1,7 +1,7 @@
 
 get_measurement_types <- function(from_file = TRUE) {
   if (from_file == TRUE) {
-    units <<- read_rds(here("data", "derived", "units.rds"))
+    ingredient_units <<- read_rds(here("data", "derived", "units.rds"))
     measures_collapsed <<- read_rds(here("data", "derived", "measurement_types.rds"))
     abbrev_dict <<- read_feather(here("data", "derived", "abbrev_dict.feather"))
     abbrev_dict_w_accepted <<- read_feather(here("data", "derived", "abbrev_dict_w_accepted.feather"))
